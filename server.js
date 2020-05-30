6e0f5085d66756b7edfaa17bd410a33d9c58061a
yhtseng1101 <yhtseng1101@gmail.com>
2020-05-30 18:42:55 -0500

    Week 1 Workshop Assignment

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+// 2. still using the app.use of express, but insert morgan middleware
+// this will configure morgan to log using the development version, which wil print some additional information 
+// we will then let morgan handle the console.log(req.headers)
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
+// __dirname is a special varaible name in node, it will refer to the absolute path of the current directory
+app.use(express.static(__dirname + '/public'));
+
+// 1. with express: app.use() takes a callback middleware function
+app.use((req, res) => {
+    // console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
